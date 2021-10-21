321af0ca273da105b400cf304823c34f7f2773bc
Matt Rossetti <MattRossetti19@GMail.com>
2021-10-20 22:38:37 -0500

    added start/stop game and play buttons

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -58,6 +58,8 @@ let playRound = (playerMove, computerMove) => {
         resultArr[0] = '2';
         resultArr[1] += 'You lose :( Rock crushes Scissors'
     }
+    console.log('playing')
+    console.log(resultArr[1], resultArr[2])
     return resultArr
 }
 
@@ -144,6 +146,142 @@ let playGame = (winRounds) => {
     gameText += `Final score is...\n${score}`
     return gameText
 }
+
+let clearYesNoButtons = () => {
+    const buttonContainer = document.querySelector('.buttonContainer');
+    buttonContainer.remove()
+}
+
+let noButtonResponse = () => {
+    const noButtonText = document.createElement('h3');
+    noButtonText.setAttribute('style', 'white-space: pre')
+    noButtonText.textContent = `Okay.\n\nHave a nice Day then.`;
+    gameArea.appendChild(noButtonText);
+}
+{/* <input type="number" placeholder="Rounds" min="1" max="10" id="roundInput"></input> */}
+let askForRounds = () => {
+    const roundsPrompt = document.createElement('h3');
+    const roundsInput = document.createElement('input');
+    const playButton = document.createElement('button');
+    playButton.addEventListener('click,', function () {
+        console.log(1);
+    });
+    roundsPrompt.setAttribute('style', 'white-space: pre');
+    roundsPrompt.textContent = 'How many rounds would you like to play?\n\nPlease enter a number between 1 and 10.\n\n';
+    roundsInput.setAttribute('type', 'number');
+    roundsInput.setAttribute('placeholder', 'Rounds')
+    roundsInput.setAttribute('min', '1')
+    roundsInput.setAttribute('max', '10')
+    roundsInput.setAttribute('id', 'roundsInput');
+    roundsInput.setAttribute('value', '5')
+    playButton.classList.add('playButton')
+    playButton.setAttribute('id', 'playButton')
+    playButton.textContent = 'Play!'
+    gameArea.appendChild(roundsPrompt);
+    gameArea.appendChild(roundsInput);
+    gameArea.appendChild(playButton);
+}
+
+let startGame = () => {
+    rounds = parseInt(document.getElementById('roundsInput').value)
+    valid = isRoundsValid(1, 10, rounds);
+    console.log(valid)
+
+    if (valid == false) {
+        alert('Number of rounds not Valid, please try again');
+        return;
+    }
+
+    while (gameArea.firstChild) {
+        gameArea.removeChild(gameArea.firstChild);
+    }
+    buildPlayArea();
+}
+
+let buildPlayArea = () => {
+    // updateGameScores()
+    const score = document.createElement('h3');
+    const gameCommentary = document.createElement('h5');
+    const movePrompt = document.createElement('h3');
+    const rockButton = document.createElement('button');
+    const paperButton = document.createElement('button');
+    const scissorsButton = document.createElement('button');
+    
+    score.textContent = `the score is ${playerWins} to ${computerWins}`;
+    gameCommentary.textContent = getGameCommentary();
+    movePrompt.textContent = 'What is your next move?'
+
+
+    rockButton.setAttribute('class', 'picButton');
+    rockButton.setAttribute('id', 'rockButton');
+    const rockPic = document.createElement('img');
+    rockPic.setAttribute('id', 'rockPic');
+    rockPic.setAttribute('src','imgs/rock.png')
+    rockPic.setAttribute('alt', 'Rock')
+    rockButton.appendChild(rockPic);
+
+    paperButton.setAttribute('class', 'picButton');
+    paperButton.setAttribute('id', 'paperButton');
+    const paperPic = document.createElement('img');
+    paperPic.setAttribute('id', 'paperPic');
+    paperPic.setAttribute('src','imgs/paper.png');
+    paperPic.setAttribute('alt', 'Paper');
+    paperButton.appendChild(paperPic)
+
+    scissorsButton.setAttribute('class','picButton');
+    scissorsButton.setAttribute('id', 'scissorsButton');
+    const scissorsPic = document.createElement('img');
+    scissorsPic.setAttribute('id', 'scissorsPic');
+    scissorsPic.setAttribute('src', 'imgs/scissors.png');
+    scissorsPic.setAttribute('alt', 'Scissors');
+    scissorsButton.appendChild(scissorsPic);
+    
+    gameArea.appendChild(score);
+    gameArea.appendChild(gameCommentary);
+    gameArea.appendChild(movePrompt);
+    moveContainer.appendChild(rockButton);
+    moveContainer.appendChild(paperButton);
+    moveContainer.appendChild(scissorsButton);
+}
+
+let getGameCommentary = () => {
+    if (playerWins == computerWins) return 'tie game.'
+    else if (playerWins > computerWins) return 'You are winning. suprisingly...'
+    else return 'You are losing, try harder.'
+}
+
+
+
 /* ---------- Main ---------- */
-console.log('done')
-// console.log(playGame(getWinRounds()))
+const yesButton = document.getElementById('yesButton');
+const noButton = document.getElementById('noButton');
+const body = document.querySelector('body');
+const gameArea = document.querySelector('.gameArea');
+const footer = document.querySelector('.footer');
+const moveContainer = document.querySelector('.moveContainer')
+let rounds = 0;
+let currentRound = 0;
+let playerWins = 0;
+let computerWins = 0;
+
+
+yesButton.addEventListener('click', clearYesNoButtons);
+yesButton.addEventListener('click', askForRounds);
+noButton.addEventListener('click', clearYesNoButtons);
+noButton.addEventListener('click', noButtonResponse);
+
+document.querySelector('body').addEventListener('click', function (e) {
+    if (e.target.id == 'playButton') startGame();
+    else if (e.target.id == 'rockButton' || e.target.id == 'rockPic') {
+        playRound('rock', getComputerMove());
+    }
+    else if (e.target.id == 'paperButton' || e.target.id == 'paperPic') {
+        playRound('paper', getComputerMove());
+    }
+    else if (e.target.id == 'scissorsButton' || e.target.id == 'scissorsPic') {
+        playRound('scissors', getComputerMove());
+    }
+});
+
+
+
